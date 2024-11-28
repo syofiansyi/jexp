9510ca2a4b3c6009963c66af508b91d6be4b9d9e
Syofian Irwanda <syofian.irwanda@iconpln.co.id>
2024-11-28 16:54:55 +0700

    d

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
-// Definisikan route untuk GET request di root URL
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Tentukan port yang akan digunakan
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+// Ekspor handler untuk digunakan oleh Vercel
+module.exports = (req, res) => {
+  app(req, res);
+};
